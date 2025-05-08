14c4f3f46bda10c52c866bf019600512759569aa
guilherme096 <guilherme.rosa60@gmail.com>
2025-05-08 10:10:15 +0100

    add springboot template

---
+++ src/main/java/elytra/stations_management/StationsManagementApplication.java
@@ -0,0 +1,13 @@
+package elytra.stations_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StationsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StationsManagementApplication.class, args);
+	}
+
+}
