750ca294612538867af8d54ee643527dfeb4fd83
ruimachado23 <rui.faria.machado@gmail.com>
2025-05-22 01:56:27 +0100

    bugfix: cors mappings

--- src/main/java/elytra/stations_management/config/WebConfig.java
+++ src/main/java/elytra/stations_management/config/WebConfig.java
@@ -3,6 +3,7 @@ package elytra.stations_management.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,5 +17,15 @@ public class WebConfig implements WebMvcConfigurer {
                 .defaultContentType(MediaType.APPLICATION_JSON)
                 .ignoreAcceptHeader(true)
                 .useRegisteredExtensionsOnly(true);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*") 
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(false) 
+            .maxAge(3600); 
     }
 }
